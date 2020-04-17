e585b7e125cc5f00dc3f28496a0f730259b28d35
Luísa Jacomini <luisa.jacomini@gmail.com>
2020-04-17 13:59:20 -0300

    paginas professor controllers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var EadController = require('../controllers/EadController');
+
+/* GET home page - index */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+
+/* GET home page - professor*/
+
+router.get('/professor/inicio', EadController.profInicio);
+router.get('/professor/postar-nota', EadController.profNotas);
+router.get('/professor/postar-tarefa', EadController.profPostarTarefa);
+router.get('/professor/postar-presenca', EadController.profPostarPresenca);
+router.get('/professor/criar-sala', EadController.profCriarSala);
+router.get('/professor/cadastrar-aluno', EadController.profCadastrarAluno);
+
+
+
+module.exports = router; 
