d868d22abf7d062a43a6459d9e9c187ed2cd637c
mh-fs <matheus.henrique1021@gmail.com>
2020-04-20 01:15:48 -0300

    adicionado router do adm

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var indexRouter = require('./routes/index');
 var AlunoRouter = require('./routes/AlunoRouter');
 var cadastroRouter = require('./routes/CadastroRouter');
 var loginRouter = require('./routes/LoginRouter');
+var admRouter = require('./routes/AdmRouter');
 
 var app = express();
 
@@ -35,6 +36,7 @@ app.use('/', indexRouter);
 app.use('/', AlunoRouter);
 app.use('/cadastro', cadastroRouter);
 app.use('/', loginRouter);
+app.use('/', admRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res) => {
