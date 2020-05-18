533d56d423f5a0c942c5befa384a58b55338db52
Adrián Ricardo Iazbeck Scalia <adrian.r.scalia@gmail.com>
2020-05-18 12:37:57 -0300

    adicione recados em models

---
+++ models/Recados.js
@@ -0,0 +1,52 @@
+let Recados = (sequelize, Datatypes) => {
+    let recados = sequelize.define(
+        'Recados', 
+        {
+            id:{
+                type: Datatypes.INTEGER,
+                primaryKey: true,
+                allowNull: false,
+                autoIncrement: true
+            },
+            titulo:{
+                type: Datatypes.STRING, 
+                allowNull: false
+            },
+            descricao:{
+                type: Datatypes.STRING, 
+                allowNull: false
+            },
+            data_entrega:{
+                type: Datatypes.DATE,
+                allowNull: false
+            },
+            data_criacao:{
+                type: Datatypes.DATE,
+                allowNull: false
+            },
+            id_classe:{
+                type: Datatypes.INTEGER,
+                allowNull: false
+            }
+        },
+        {
+            tableName: "Recados",
+            timestamps: true
+        }
+    )
+
+    Recados.associate = (models) => {
+        Recados.belongsTo(
+            models.Classes,
+            {
+                foreignKey: 'id_classes',
+                as: 'classes'
+            }
+        );
+    }
+
+    return recados;
+
+}
+
+module.exports = Recados;
