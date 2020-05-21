46b7e66e502a70881712dbe82a628553df4f3539
mh-fs <matheus.henrique1021@gmail.com>
2020-05-21 01:32:10 -0300

    Verifique se o usuario ja esta cadastrado

--- controllers/CadastroController.js
+++ controllers/CadastroController.js
@@ -17,55 +17,105 @@ module.exports = {
 
         const {name, email, password} = req.body;
 
-        bcrypt.hash(password, 10, (error, hash) => {
-
-            if(error) {return res.status(500).send({error: error})}
-
-            models.Usuario.create(
+        models.Aluno.findAll(
             {
+                attributes: ["id_usuario"],
+                include: [
+                    {
+                        model: models.Usuario,
+                        as: "usuarioAluno",
+                        required: true,
+                        where: { email: email }
+                    }
+                ]
+            }
+        )
+        .then(query => {
 
-                nome: name,
-                email: email,
-                senha: hash
+            if(query.length > 0){
 
-            })
-            .then(user => models.Aluno.create({
-                id_usuario: user.id
-            }))
-            .then(response => res.status(201).send({
-                data: response,
-                message: "Aluno cadastrado."
-            }))
-            .catch(error => res.status(500).send(error));
+                res.status(409).send({
+                    message: "Aluno já cadastrado!"
+                });
 
-        });
+            }else{
+
+                bcrypt.hash(String(password), 10, (error, hash) => {
     
+                    if(error) return res.status(500).send({error: error});
+        
+                    models.Usuario.create(
+                    {
+        
+                        nome: name,
+                        email: email,
+                        senha: hash
+        
+                    })
+                    .then(user => models.Aluno.create({
+                        id_usuario: user.id
+                    }))
+                    .then(response => res.status(201).send({
+                        data: response,
+                        message: "Aluno cadastrado."
+                    }))
+                    .catch(error => res.status(503).send(error));
+                });
+            }
+        })
+        .catch(error => res.status(500).send(error));
     },
     cadastrarProfessor: (req, res, next) => {
 
         const {name, email, password} = req.body;
 
-        bcrypt.hash(password, 10, (error, hash) => {
+        models.Professor.findAll(
+            {
+                attributes: ["id_usuario"],
+                include: [
+                    {
+                        model: models.Usuario,
+                        as: "usuarioProfessor",
+                        required: true,
+                        where: { email: email }
+                    }
+                ]
+            }
+        )
+        .then(query => {
 
-            if(error) {return res.status(500).send({error: error})}
+            if(query.length > 0){
 
-            models.Usuario.create({
+                res.status(409).send({
+                    message: "Professor já cadastrado!"
+                });
 
-                nome: name,
-                email: email,
-                senha: hash
+            }else{
+
+                bcrypt.hash(String(password), 10, (error, hash) => {
     
-            })
-            .then(user => models.Professor.create({
-                id_usuario: user.id
-            }))
-            .then(response => res.status(201).send({
-                data: response,
-                message: "Professor cadastrado."
-            }))
-            .catch(error => res.status(500).send(error));
-
-        });
+                    if(error) return res.status(500).send({error: error});
+        
+                    models.Usuario.create(
+                    {
+        
+                        nome: name,
+                        email: email,
+                        senha: hash
+        
+                    })
+                    .then(user => models.Professor.create({
+                        id_usuario: user.id
+                    }))
+                    .then(response => res.status(201).send({
+                        data: response,
+                        message: "Professor cadastrado."
+                    }))
+                    .catch(error => res.status(503).send(error));
+                });
+            }
+        })
+        .catch(error => res.status(500).send(error));
     
     }
 
