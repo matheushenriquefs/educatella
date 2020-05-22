55c9981ed590c4d5e1793b3bd17f218a8ec4a9db
mh-fs <matheus.henrique1021@gmail.com>
2020-05-22 01:46:23 -0300

    Adicione middleware para validar o token

---
+++ middlewares/authUser.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+module.exports = (req, res, next) => {
+
+    if(req.headers.authorization === undefined){
+        
+        return res.status(401).render('not-found');
+
+    }else{
+
+        const token = req.headers.authorization.split(" ")[1];
+
+        try {
+
+            const decode = jwt.verify(token, process.env.JWT_KEY);
+            req.usuario = decode;
+            next();
+            
+        } catch (error) {
+            
+            return res.status(401).send({ title: "Erro!", message: "Falha na autenticação!" });
+    
+        }
+
+    }
+
+}
