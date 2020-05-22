cc456b5ab8e7e834a0478bf0f7bace5a3f1f209e
mh-fs <matheus.henrique1021@gmail.com>
2020-05-22 01:47:59 -0300

    Adicione middleware para validar as rotas

--- routes/ProfessorRouter.js
+++ routes/ProfessorRouter.js
@@ -5,12 +5,15 @@ var ProfessorController = require('../controllers/ProfessorController');
 
 /* Rotas Professor */
 
-router.get('/professor/inicio', ProfessorController.profInicio);
-router.get('/professor/recados', ProfessorController.profRecados);
-router.get('/professor/postar-nota', ProfessorController.profNotas);
-router.get('/professor/postar-tarefa', ProfessorController.profPostarTarefa);
-router.get('/professor/postar-presenca', ProfessorController.profPostarPresenca);
-router.get('/professor/criar-sala', ProfessorController.profCriarSala);
-router.get('/professor/gerenciar-aluno', ProfessorController.profGerenciarAluno);
+// Middlewares
+const authUser = require("../middlewares/authUser");
+
+router.get('/professor/inicio', authUser, ProfessorController.profInicio);
+router.get('/professor/recados', authUser, ProfessorController.profRecados);
+router.get('/professor/postar-nota', authUser, ProfessorController.profNotas);
+router.get('/professor/postar-tarefa', authUser, ProfessorController.profPostarTarefa);
+router.get('/professor/postar-presenca', authUser, ProfessorController.profPostarPresenca);
+router.get('/professor/criar-sala', authUser, ProfessorController.profCriarSala);
+router.get('/professor/gerenciar-aluno', authUser, ProfessorController.profGerenciarAluno);
 
 module.exports = router; 
