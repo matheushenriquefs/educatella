f709472699af5ecac1e33d7a2f2222ea5461f662
mh-fs <matheus.henrique1021@gmail.com>
2020-05-24 18:47:00 -0300

    Adicione autenticacao nas rotas

--- routes/ProfessorRouter.js
+++ routes/ProfessorRouter.js
@@ -8,14 +8,14 @@ const authUser = require("../middlewares/authUser");
 
 /* Rotas Professor */ /*tirar authUser pra desenvolver*/
 
-router.get('/professor/inicio', ProfessorController.profInicio);
-router.get('/professor/recados',  ProfessorController.profRecados);
-router.get('/professor/criar-recado', ProfessorController.profRecadosCriar);
-router.post('/professor/criar-recado', ProfessorController.profRecadosCriar2);
-router.get('/professor/apagar-recado',  ProfessorController.profRecadosApagar);
-router.post('/professor/apagar-recado/:id',  ProfessorController.profRecadosApagar2);
-router.get('/professor/editar-recado',  ProfessorController.profRecadosEditar);
-router.post('/professor/editar-recado/:id',  ProfessorController.profRecadosEditar2);
+router.get('/professor/inicio', authUser, ProfessorController.profInicio);
+router.get('/professor/recados', authUser,  ProfessorController.profRecados);
+router.get('/professor/criar-recado', authUser, ProfessorController.profRecadosCriar);
+router.post('/professor/criar-recado', authUser, ProfessorController.profRecadosCriar2);
+router.get('/professor/apagar-recado', authUser,  ProfessorController.profRecadosApagar);
+router.post('/professor/apagar-recado/:id', authUser,  ProfessorController.profRecadosApagar2);
+router.get('/professor/editar-recado', authUser,  ProfessorController.profRecadosEditar);
+router.post('/professor/editar-recado/:id', authUser,  ProfessorController.profRecadosEditar2);
 router.get('/professor/postar-nota', authUser, ProfessorController.profNotas);
 router.get('/professor/postar-tarefa', authUser, ProfessorController.profPostarTarefa);
 router.get('/professor/postar-presenca', authUser, ProfessorController.profPostarPresenca);
