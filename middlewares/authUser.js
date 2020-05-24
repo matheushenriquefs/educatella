87878821d2e7a8b83a29ad34073e41ee92b04bb6
mh-fs <matheus.henrique1021@gmail.com>
2020-05-24 18:51:37 -0300

    Mude autenticacao para cookies

--- middlewares/authUser.js
+++ middlewares/authUser.js
@@ -3,16 +3,15 @@ require("dotenv").config();
 
 module.exports = (req, res, next) => {
 
-    if(req.headers.authorization === undefined){
+    if(req.cookies.token === undefined){
         
         return res.status(401).render('not-found');
 
     }else{
-
-        const token = req.headers.authorization.split(" ")[1];
-
+        
         try {
 
+            const {token} = req.cookies;
             const decode = jwt.verify(token, process.env.JWT_KEY);
             req.usuario = decode;
             next();
