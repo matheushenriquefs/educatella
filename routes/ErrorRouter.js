1282026b5a430d5b3928980c5722f3d5cbfcde4b
Matheus Henrique <matheushenrique@MacBook-Air-de-Matheus.local>
2020-07-05 15:47:35 -0300

    T3-Matheus adicione novas rotas de erro

--- routes/ErrorRouter.js
+++ routes/ErrorRouter.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const controller = require("../controllers/ErrorController");
 
 router.get("/error/401", controller.renderError401);
+router.get("/error/401-aluno", controller.renderError401Aluno);
+router.get("/error/401-professor", controller.renderError401Professor);
 
 module.exports = router;
