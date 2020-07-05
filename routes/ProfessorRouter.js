5a4a8afc4f3eaa52c241f96729c0262d17fbd1e9
Matheus Henrique <matheushenrique@MacBook-Air-de-Matheus.local>
2020-07-05 16:31:34 -0300

    T3-Matheus Remova arquivos inutilizados

--- routes/ProfessorRouter.js
+++ routes/ProfessorRouter.js
@@ -30,51 +30,51 @@ const authUserType = require("../middlewares/authUserTypeProfessor");
 /* Rotas Professor */ /*tirar authUser pra desenvolver*/
 
 //Início e criar classe
-router.get('/professor/inicio', authUser, authUserType, ProfessorController.profInicio);
-router.post('/professor/inicio', authUser, authUserType, ProfessorController.criarClasse);
-router.post('/professor/acessar-classe', authUser, authUserType, ProfessorController.acessarClasse);
-router.post('/professor/editar-classe', authUser, authUserType, ProfessorController.updateClasse);
-router.post('/professor/delete-classe', authUser, authUserType, ProfessorController.destroyClasse);
+router.get('/professor/inicio', authUser, ProfessorController.profInicio);
+router.post('/professor/inicio', authUser, ProfessorController.criarClasse);
+router.post('/professor/acessar-classe', authUser, ProfessorController.acessarClasse);
+router.post('/professor/editar-classe', authUser, ProfessorController.updateClasse);
+router.post('/professor/delete-classe', authUser, ProfessorController.destroyClasse);
 
 //RECADOS 
-router.get('/professor/recados', authUser, authUserType,  ProfessorController.profRecados);
+router.get('/professor/recados', authUser,  ProfessorController.profRecados);
 //RECADOS CRIAR
-router.get('/professor/criar-recado', authUser, authUserType,ProfessorController.profRecadosCriar);
+router.get('/professor/criar-recado', authUser,ProfessorController.profRecadosCriar);
 
-router.post('/professor/criar-recado', authUser, authUserType,
+router.post('/professor/criar-recado', authUser,
           [check ("titulo").isLength({min:1}),
           check ("descricao").isLength({min:1})]
           ,ProfessorController.profRecadosCriar2);
 //apagar Recados          
-router.get('/professor/apagar-recado', authUser, authUserType, ProfessorController.profRecadosApagar);
-router.post('/professor/apagar-recado/:id', authUser, authUserType, ProfessorController.profRecadosApagar2);
+router.get('/professor/apagar-recado', authUser, ProfessorController.profRecadosApagar);
+router.post('/professor/apagar-recado/:id', authUser, ProfessorController.profRecadosApagar2);
 
 //Editar Recados
-router.get('/professor/editar-recado', authUser, authUserType, ProfessorController.profRecadosEditar);
-router.post('/professor/editar-recado/:id', authUser, authUserType,
+router.get('/professor/editar-recado', authUser, ProfessorController.profRecadosEditar);
+router.post('/professor/editar-recado/:id', authUser,
             [check ("titulo").isLength({min:1}),
             check ("descricao").isLength({min:1})]
             ,ProfessorController.profRecadosEditar2);
 
 //Gerenciar Notas            
-router.get('/professor/gerenciar-nota', authUser, authUserType, ProfessorController.gerenciarNota);
-router.post('/professor/gerenciar-nota-aluno/:id', authUser, authUserType, ProfessorController.gerenciarNotaAluno);
-router.post('/professor/poner-nota-aluno/:id', authUser, authUserType, ProfessorController.ponerNotaAluno);
-router.post('/professor/poner-nota-aluno-tarefa/:id', authUser, authUserType, ProfessorController.ponerNotaAlunoTarefa);
+router.get('/professor/gerenciar-nota', authUser, ProfessorController.gerenciarNota);
+router.post('/professor/gerenciar-nota-aluno/:id', authUser, ProfessorController.gerenciarNotaAluno);
+router.post('/professor/poner-nota-aluno/:id', authUser, ProfessorController.ponerNotaAluno);
+router.post('/professor/poner-nota-aluno-tarefa/:id', authUser, ProfessorController.ponerNotaAlunoTarefa);
 //TAREFAS
-router.post('/professor/postar', authUser, authUserType, upload.any(), ProfessorController.addTarefa);
-router.post('/professor/editar', authUser, authUserType, ProfessorController.update);
-router.post('/professor/delete', authUser, authUserType, ProfessorController.destroy);
-router.post('/professor/tarefa', authUser, authUserType, ProfessorController.tarefaMenu); //rota para acessar classes
+router.post('/professor/postar', authUser, upload.any(), ProfessorController.addTarefa);
+router.post('/professor/editar', authUser, ProfessorController.update);
+router.post('/professor/delete', authUser, ProfessorController.destroy);
+router.post('/professor/tarefa', authUser, ProfessorController.tarefaMenu); //rota para acessar classes
 //GERENCIAR Aluno
-router.get('/professor/gerenciar-aluno', authUser, authUserType, ProfessorController.profGerenciarAluno);
-router.post('/professor/gerenciar-aluno/:id', authUser, authUserType, ProfessorController.profGerenciarAluno1);
+router.get('/professor/gerenciar-aluno', authUser, ProfessorController.profGerenciarAluno);
+router.post('/professor/gerenciar-aluno/:id', authUser, ProfessorController.profGerenciarAluno1);
 //Alterar informações usuário
-router.post('/professor/alterarImagem', uploadImagem.single("img"), authUser, authUserType, ProfessorController.alterarImagem);
-router.post('/professor/alterarNome', authUser, authUserType, ProfessorController.alterarNome);
-router.post('/professor/alterarEmail', authUser, authUserType, ProfessorController.alterarEmail);
-router.post('/professor/alterarSenha', authUser, authUserType, ProfessorController.alterarSenha);
-router.post('/professor/gerenciarNotasTarefas', authUser, authUserType, ProfessorController.gerenciarNotasTarefas);
+router.post('/professor/alterarImagem', uploadImagem.single("img"), authUser, ProfessorController.alterarImagem);
+router.post('/professor/alterarNome', authUser, ProfessorController.alterarNome);
+router.post('/professor/alterarEmail', authUser, ProfessorController.alterarEmail);
+router.post('/professor/alterarSenha', authUser, ProfessorController.alterarSenha);
+router.post('/professor/gerenciarNotasTarefas', authUser, ProfessorController.gerenciarNotasTarefas);
 
 
 module.exports = router; 
