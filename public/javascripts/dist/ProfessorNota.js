76bfb2007636d23d8499a20f31243924bf30c1a9
matheus-reyes <matheus-reyes@usp.br>
2020-07-11 19:37:58 -0300

    Corrigindo Feedback Notas

--- public/javascripts/dist/ProfessorNota.js
+++ public/javascripts/dist/ProfessorNota.js
@@ -1,11 +1,17 @@
-let feedbackNota = document.getElementById("feedbackNota").value;
-let mensagemFeedbackNota = document.getElementById("mensagemFeedbackNota");
+let formNotas = document.getElementsByClassName("formNotas");
+let feedbackNotas = document.getElementsByClassName("feedbackAplicarNota");
+let selectNotas = document.getElementsByClassName("selectNota");
 
-function acessarNotaFeedback(){
-    if(feedbackNota != "inicio"){
-        $('#modal-feedback-nota').modal('show');
-        mensagemFeedbackNota.innerText = feedbackNota;
-    }
+for(let i = 0; i < formNotas.length; i++){
+    selectNotas[i].addEventListener("change", evento => {
+
+        evento.preventDefault();
+        feedbackNotas[i].innerText = "Nota aplicada!";
+        
+        setTimeout(()=>{
+            feedbackNotas[i].innerText = '';
+            formNotas[i].submit();
+        }, 1000);
+
+    });
 }
-
-acessarNotaFeedback();
